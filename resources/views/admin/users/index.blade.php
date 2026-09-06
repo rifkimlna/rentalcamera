f98b3c67e444d3a89053aa7c1befc7aee0f2a734
rifkimlna <rfkimaulana01@gmail.com>
2026-09-06 13:14:09 +0700

    Sederhanakan avatar tabel user: kepala bulat + badan

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -119,7 +119,7 @@
                         <td>
                             <div class="flex items-center gap-2">
                                 <div class="bg-[#f5f5f7] rounded-full flex items-center justify-center w-8 h-8">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-[#6e6e73]" viewBox="0 0 24 24" fill="currentColor"><path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clip-rule="evenodd"/></svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-[#6e6e73]" viewBox="0 0 24 24" fill="currentColor"><circle cx="12" cy="8" r="4"/><path d="M4 21c0-4 3.6-7 8-7s8 3 8 7v1H4v-1z"/></svg>
                                 </div>
                                 <div>
                                     <div class="font-medium">{{ $user->nama }}</div>
